8416c108d8b404bcc3c17a88a0fb48885ea87511
Palash <palashchiplunkar51@gmail.com>
2022-04-06 22:48:45 +0530

    commit3

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ import {
 const App = () => {
   return (
     <View>
-      <Text>Hello world This is a template</Text>
+      <Text>Hello world This is a template for starting</Text>
     </View>
   );
 };
