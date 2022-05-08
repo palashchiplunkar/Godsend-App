fbc8bc88653541e220371faf5dd2b2a1670d220a
Palash <palashchiplunkar51@gmail.com>
2022-05-08 12:46:04 +0530

    commit

--- App.js
+++ App.js
@@ -10,15 +10,35 @@ import {
   View,
 } from 'react-native';
 
-
 const App = () => {
   return (
-    <View>
-      <Text>Hello world ,This is a template for starting your app</Text>
+    <View style={styles.container}>
+      <View style={styles.loginContainer}>
+        <Text style={styles.login}>Login</Text>
+      </View>
     </View>
   );
 };
 
-
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#1a424d',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginContainer: {
+    backgroundColor: 'white',
+    width: '75%',
+    height: '50%',
+    borderRadius: 20,
+  },
+  login: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
 
 export default App;
